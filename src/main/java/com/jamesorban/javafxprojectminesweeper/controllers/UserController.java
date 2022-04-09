e5d8a3cbe2594d618d324d8f2ad3e559887fad86
orbanjames <orbanjams4@gmail.com>
2022-04-10 01:04:35 +0200

    connecting to postgreSQL

---
+++ src/main/java/com/jamesorban/javafxprojectminesweeper/controllers/UserController.java
@@ -0,0 +1,27 @@
+package com.jamesorban.javafxprojectminesweeper.controllers;
+
+import com.jamesorban.javafxprojectminesweeper.models.UserAccount;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class UserController {
+
+    @FXML
+    private Button submitButton;
+    @FXML
+    private TextField email;
+    @FXML
+    private TextField name;
+    @FXML
+    private PasswordField password;
+ public void getData(ActionEvent actionEvent){
+    System.out.println(name.getText());
+    System.out.println(email.getText());
+    System.out.println(password.getText());
+    UserAccount.writeToDatabase(name.getText(), email.getText(), password.getText());
+ }
+
+}
---
+++ src/main/java/com/jamesorban/javafxprojectminesweeper/models/PreparedStatement.java
@@ -0,0 +1,9 @@
+package com.jamesorban.javafxprojectminesweeper.models;
+
+public class PreparedStatement {
+    public void setString(int i, String username) {
+    }
+
+    public void executeUpdate() {
+    }
+}
