a8543f079d44443acc97d5c8433b459e4cfad093
orbanjames <orbanjams4@gmail.com>
2022-04-13 03:01:18 +0200

    subsequent commits and update

--- src/main/java/com/jamesorban/javafxprojectminesweeper/models/PreparedStatement.java
+++ src/main/java/com/jamesorban/javafxprojectminesweeper/models/PreparedStatement.java
@@ -1,7 +1,7 @@
 package com.jamesorban.javafxprojectminesweeper.models;
 
 public class PreparedStatement {
-    public void setString(int i, String username) {
+    public void setString(int id, String username) {
     }
 
     public void executeUpdate() {
